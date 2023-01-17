65dc775ed86f021800e6c9b2c616d960b8c29155
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-17 13:11:57 +0700

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Какое число большее?
+
+Console.WriteLine("Введите первое число {a}: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число {b}: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) Console.WriteLine("Число {a} большее, а число {b} меньшее");
+
+else
+{
+    Console.WriteLine("Число {b} большее, а число {a} меньшее");
+}
