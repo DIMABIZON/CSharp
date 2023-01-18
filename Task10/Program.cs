091e0218888cd33e72257ac93f5c1acbff8f0c83
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-18 20:17:16 +0700

    Task 10 simply

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// вторая цифра трёхзначного числа
+
+Console.Write("Введите трёхзначное число -->");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int d = (n/10) % 10;
+
+Console.Write("d = ");
+Console.WriteLine(d);
