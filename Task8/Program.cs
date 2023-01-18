60f7db9b332ecb5de6c9d70469d9edb12453fcee
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-18 14:03:53 +0700

    task8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Чётные от 1 до N
+
+Console.Write("Введите число N-->");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+
+while (i < n)
+{
+    i += 2;
+    Console.Write($"{i}, ");
+    if (i == n-1) break;
+}
