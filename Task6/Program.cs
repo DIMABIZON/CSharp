0b4179facf3b0f6ca6a01cae7febcbad95a23eca
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-18 12:45:28 +0700

    task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Чётное ли число?
+
+Console.WriteLine("-->");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) Console.WriteLine("Введённое вами число Чётное");
+
+else
+{
+   Console.WriteLine("Введённое вами число Нечётное"); 
+}
