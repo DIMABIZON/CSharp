b2d3eead5b3bfb588418a77973bf9c8140f722ce
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-20 11:24:08 +0700

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 15: выходной день!
+
+Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <=5)
+{
+    Console.WriteLine("нет, это не выходной день");
+}
+else
+{
+    Console.WriteLine("да, это выходной день!!!");
+}
+
