a7e9a823533e398d4644c72af2b5a17dcd2d5582
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-22 13:15:44 +0700

    Task19

---
+++ Seminar3/Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 19: Пятизначный полиндром?
+
+Console.WriteLine("Введите пятизначное число --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string palindrom = a.ToString(); //преобразование целого числа в строку
+
+if (palindrom[0]==palindrom[4] && palindrom[1]==palindrom[3]) //ускоренная проверка логическим И условия палиндрома
+{
+    Console.Write("Поздравляю, у вас палиндром! ");
+}
+else
+{
+    Console.WriteLine("нет, ваше число не палиндром. Введите зеркальное число");
+}
