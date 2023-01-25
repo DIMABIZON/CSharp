d08c6b4de85559f2139a4721cdaf5af37831ff3d
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-25 21:34:27 +0700

    Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 25: возвести часло А в степень В
+
+Console.WriteLine("Введите число А --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В --> ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int proizved = a * a;
+
+for (int i = 1; i <= b - 2; i++)
+{
+     proizved = proizved * a;
+}
+
+Console.Write("Ваше число А в степени В равно: ");
+Console.WriteLine(proizved);
