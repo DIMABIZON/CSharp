d42f2097102325f7110fe34b6fc1459c4aaf3128
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-30 20:56:30 +0700

    Task36

---
+++ Seminar5/Task36/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 36: Cумма элементов, стоящих на нечетных позициях одномерного массива
+
+Console.WriteLine("Сколько чисел будет в вашем массиве?: --> ");
+int kol = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [kol];
+Random rnd = new Random (); 
+
+for (int i = 0; i < kol; i++)              
+{
+    array [i] = rnd.Next(-99, 100);
+    Console.Write(array [i] + " ");     
+}
+int summa = 0;
+
+for (int i = 1; i < kol; i += 2)
+{
+    summa = summa + array [i];
+}
+Console.Write("\n" + $"Сумма элементов, стоящих на нечётных позициях в массиве: {summa}");
