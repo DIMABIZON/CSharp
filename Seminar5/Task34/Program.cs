eae0ad2bd156ee4e9ee69c516cefccf44725be66
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-01-30 20:59:17 +0700

    Task34

---
+++ Seminar5/Task34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34: Количество четных чисел случайного массива положительных трехзначных чисел
+
+Console.WriteLine("Введите желаемое количество чисел массива --> ");
+int kol = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [kol];
+Random rand = new Random (); 
+
+for (int i = 0; i < kol; i++)              
+{
+    array [i] = rand.Next(100,1000);
+    Console.Write($"{array [i]}, ");     
+}
+
+Console.WriteLine ();
+
+int count = 0;
+
+for (int i = 0; i < kol; i++)              
+{
+    if (array [i] % 2 == 0)
+    {
+        count ++;
+          
+    } 
+}
+Console.Write($"Количество чётных чисел в массиве равно: {count}");
