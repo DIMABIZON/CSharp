9a27d7bcc3df13896569e233d29b33919226b273
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-02-10 11:00:02 +0600

    Task54

---
+++ Seminar8/Task54/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Random rnd = new Random();
+int m = rnd.Next(3, 4);
+int n = rnd.Next(4, 5);
+
+int [,] array = new int [m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array [i,j] = rnd.Next(1, 10);
+        Console.Write(String.Format("{0,2}", array [i,j]) + " ");
+    }  
+    Console.WriteLine();
+}
+
+int index = 1;                  // Начало Гномьей сортировки массива по убыванию
+int nextIndex = index + 1;
+int temp;
+
+for (int i = 0; i < m; i++)
+{
+    while (index < n)
+    {
+        if (array [i, index - 1] > array [i, index]) // если предыдущий элемент строки больше следующего, идём дальше
+        {
+            index = nextIndex;
+            nextIndex++;
+        }
+        else                                        // иначе переприсваиваем индексы = передвигаем больший элемент влево
+        {
+            temp = array [i, index - 1];
+            array [i, index - 1] = array [i, index];
+            array [i, index] = temp;
+            index--;
+            if (index == 0)                          // и проверяем условие возврата в начало строки, а затем переходим к
+            {                                        // к следующему неотсортированному элементу
+                index = nextIndex;
+                nextIndex++;
+            }
+        }
+    }
+} 
+
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength (0); i++)
+{
+    for (int j = 0; j < array.GetLength (1); j++)
+    {
+        Console.Write(String.Format("{0,2}", array [i, j]) + " ");
+    }  
+    Console.WriteLine();
+}
