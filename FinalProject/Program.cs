f637d861db90e44f218e0cd4ac65ae794db2e792
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-02-26 08:56:53 +0600

    Разделили входную строку символов на слова

---
+++ FinalProject/Program.cs
@@ -0,0 +1,6 @@
+﻿// Из имеющегося массива строк сформировать массив из строк, длина которых меньше, либо равна 3 символа.
+
+Console.WriteLine("Введите несколько слов или чисел: --> ");
+string wordsmassiv = Convert.ToString(Console.ReadLine()); 
+char[] delimiterChars = {' ', ',', '.', ':'};
+string [] words = wordsmassiv.Split(delimiterChars);            
