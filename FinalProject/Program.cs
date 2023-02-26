7cb66ecbebd795248e50f5642c99e4553231e86a
Dmytry Zedgenizov <dimzed2001@mail.ru>
2023-02-26 09:15:43 +0600

    Создали цикл перебора слов и цикл поиска слова до 3-х символов длиной

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Введите несколько слов или чисел:
 string wordsmassiv = Convert.ToString(Console.ReadLine()); 
 char[] delimiterChars = {' ', ',', '.', ':'};
 string [] words = wordsmassiv.Split(delimiterChars);            
+
+for (int n = 0; n < words.Length; n++)
+{
+    string simbols = words [n];
+    
+    if (simbols.Length <= 3)
+    {
+        Console.Write(($"{words[n]}, "));
+    }
+}
